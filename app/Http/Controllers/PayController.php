7003021892b8dee7d4cfb813ccff0d91967389f0
elida <elidakirigo@gmail.com>
2024-10-10 00:40:57 +0300

    -

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -8,8 +8,19 @@ use Mpesa;
 class PayController extends Controller
 {
    public function stk(){
-  $mpesa= new \Safaricom\Mpesa\Mpesa();
-$BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks
+        $mpesa= new \Safaricom\Mpesa\Mpesa();
+        $BusinessShortCode='174379';
+        $LipaNaMpesaPasskey='';
+        $TransactionType='CustomerPaybillOnline';
+        $Amount='1';
+        $PartyA='254720467320';
+        $PartyB='174379';
+        $PhoneNumber='254720467320';
+        $CallBackURL='';
+        $AccountReference='AccountReference';
+        $TransactionDesc='TransactionDesc';
+        $Remarks='Remarks';
+
 $stkPushSimulation=$mpesa->STKPushSimulation($BusinessShortCode, $LipaNaMpesaPasskey, $TransactionType, $Amount, $PartyA, $PartyB, $PhoneNumber, $CallBackURL, $AccountReference, $TransactionDesc, $Remarks);
    }
 }
